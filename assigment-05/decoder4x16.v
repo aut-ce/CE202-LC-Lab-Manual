1888dd69ecd4ece2e3755c45ce839b5bfbf39bc2
Ali Mohammadpour <aliemohammadpour@gmail.com>
2020-09-21 11:42:49 +0330

    feat: Add Lab-05 part 2 template sources

---
+++ assigment-05/decoder4x16.v
@@ -0,0 +1,32 @@
+/*--  *******************************************************
+--  Computer Architecture Course, Laboratory Sources 
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce[dot]aut[dot]ac[dot]ir
+--  *******************************************************
+--  All Rights reserved (C) 2019-2020
+--  *******************************************************
+--  Student ID  : 
+--  Student Name: 
+--  Student Mail: 
+--  *******************************************************
+--  Additional Comments:
+--
+--*/
+
+/*-----------------------------------------------------------
+---  Module Name: Decoder 4 to 16
+---  Description: Lab 05 Part 2
+-----------------------------------------------------------*/
+`timescale 1 ns/1 ns
+
+module decoder4x16 (
+	input [3:0] in ,
+	input 		en ,
+	output [15:0] dout,	
+);
+	/* write your code here */
+	
+	/* write your code here */
+
+endmodule
---
+++ assigment-05/function4x1.v
@@ -0,0 +1,34 @@
+/*--  *******************************************************
+--  Computer Architecture Course, Laboratory Sources 
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce[dot]aut[dot]ac[dot]ir
+--  *******************************************************
+--  All Rights reserved (C) 2019-2020
+--  *******************************************************
+--  Student ID  : 
+--  Student Name: 
+--  Student Mail: 
+--  *******************************************************
+--  Additional Comments:
+--
+--*/
+
+/*-----------------------------------------------------------
+---  Module Name: Function Implementation usin Mux 4 to 16
+---  Description: Lab 05 Part 3
+-----------------------------------------------------------*/
+`timescale 1 ns/1 ns
+
+module function4x1 (
+	input a ,
+	input b ,
+	input c ,
+	input d ,
+	output f,	
+);
+	/* write your code here */
+	
+	/* write your code here */
+
+endmodule
