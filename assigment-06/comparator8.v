17d087b8cf5e88b3098e309153695afda166f992
Ali Mohammadpour <aliemohammadpour@gmail.com>
2020-09-22 20:45:18 +0330

    feat: Add Lab-06 testbench template sources

---
+++ assigment-06/comparator8.v
@@ -0,0 +1,38 @@
+/*--  *******************************************************
+--  Computer Architecture Course, Laboratory Sources 
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce[dot]aut[dot]ac[dot]ir
+--  *******************************************************
+--  All Rights reserved (C) 2019-2020
+--  *******************************************************
+--  Student ID  : 
+--  Student Name: 
+--  Student Mail: 
+--  *******************************************************
+--  Additional Comments:
+--
+--*/
+
+/*-----------------------------------------------------------
+---  Module Name: 8 Bits Comparator
+---  Description: Lab 06 Part 3
+-----------------------------------------------------------*/
+`timescale 1 ns/1 ns
+
+module comparator3 (
+	input [7:0] A ,
+	input [7:0] B ,
+	input 		l ,
+	input 		e ,
+	input 		g ,
+	output 		lt ,
+	output 		et ,
+	output 		gt
+);
+
+	/* write your code here */
+	
+	/* write your code here */
+
+endmodule
---
+++ assigment-06/tb_comparator8.v
@@ -0,0 +1,45 @@
+/*--  *******************************************************
+--  Computer Architecture Course, Laboratory Sources 
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce[dot]aut[dot]ac[dot]ir
+--  *******************************************************
+--  All Rights reserved (C) 2019-2020
+--  *******************************************************
+--  Student ID  : 
+--  Student Name: 
+--  Student Mail: 
+--  *******************************************************
+--  Additional Comments:
+--
+--*/
+
+/*-----------------------------------------------------------
+---  Module Name: Decoder Testbench
+---  Description: Lab 05 Part 1 Testbench
+-----------------------------------------------------------*/
+`timescale 1 ns/1 ns
+
+
+module tb_comparator3 ();
+
+reg [2:0] A;
+reg [2:0] B;
+reg l;
+reg e;
+reg g;
+wire lt;
+wire et;
+wire gt;
+
+	comparator3 test_comparator3 (.A(A), .B(B), .l(l), .e(e), .g(g), .lt(lt), .et(et), .gt(gt));
+
+
+	initial 
+		begin
+		
+		// write your code here
+		
+	end
+
+endmodule
