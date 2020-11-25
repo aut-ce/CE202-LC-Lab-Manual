1e379ab06e7f71a72b35b81b249e017da3f01107
Ali Mohammadpour <aliemohammadpour@gmail.com>
2020-11-25 11:18:36 +0330

    feat: Add Lab-08 template sources

---
+++ assignment-08/multiplexer4x4.v
@@ -0,0 +1,35 @@
+/*--  *******************************************************
+--  Computer Architecture Course, Laboratory Sources 
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce.aut.ac.ir
+--  *******************************************************
+--  All Rights reserved (C) 2019-2020
+--  *******************************************************
+--  Student ID  : 
+--  Student Name: 
+--  Student Mail: 
+--  *******************************************************
+--  Additional Comments:
+--
+--*/
+
+/*-----------------------------------------------------------
+---  Module Name: Multiplexer 4bit 4 to 1
+---  Description: Lab 08 Part 1
+-----------------------------------------------------------*/
+`timescale 1 ns/1 ns
+
+module multiplexer4x4 (
+	input [3:0] w3 ,
+	input [3:0] w2 ,
+	input [3:0] w1 ,
+	input [3:0] w0 ,
+	input [1:0]	sel ,
+	output [3:0] y
+);
+	/* write your code here */
+	
+	/* write your code here */
+
+endmodule
