a65f56261fd1f8267f95d6c4d202320ec0bdbf6b
Ali Mohammadpour <aliemohammadpour@gmail.com>
2020-12-12 02:37:28 +0330

    Update Lab Material

---
+++ assignment-10/system.v
@@ -0,0 +1,36 @@
+/*--  *******************************************************
+--  Computer Architecture Course, Laboratory Sources 
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce[dot]aut[dot]ac[dot]ir
+--  *******************************************************
+--  All Rights reserved (C) 2019-2020
+--  *******************************************************
+--  Student ID  : 
+--  Student Name: 
+--  Student Mail: 
+--  *******************************************************
+--  Additional Comments:
+--
+--*/
+
+/*-----------------------------------------------------------
+---  Module Name: Sequential System
+---  Description: Lab 10 Part 3
+-----------------------------------------------------------*/
+`timescale 1 ns/1 ns
+
+module dflop (
+	input        rst ,
+	input        clk ,
+	input  [3:0] din ,
+	output [3:0] dout_left ,
+	output [3:0] dout_right
+);
+
+	/* write your code here */
+	
+	/* write your code here */
+
+endmodule
+
---
+++ assignment-10/tb_system.v
@@ -0,0 +1,40 @@
+/*--  *******************************************************
+--  Computer Architecture Course, Laboratory Sources 
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce[dot]aut[dot]ac[dot]ir
+--  *******************************************************
+--  All Rights reserved (C) 2019-2020
+--  *******************************************************
+--  Student ID  : 
+--  Student Name: 
+--  Student Mail: 
+--  *******************************************************
+--  Additional Comments:
+--
+--*/
+
+/*-----------------------------------------------------------
+---  Module Name: System Testbench
+---  Description: Lab 10 Part 4
+-----------------------------------------------------------*/
+`timescale 1 ns/1 ns
+
+module tb_system ();
+
+reg        rst ;
+reg        clk ;
+reg  [3:0] din ;
+wire [3:0] dout_left ;
+wire [3:0] dout_right ;
+
+	
+	initial 
+		begin
+		
+		// write your code here
+		
+	end
+
+endmodule
+
