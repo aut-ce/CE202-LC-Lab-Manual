4dbc1965cd7eefaf0e9bd53b54cebe66a2310a1c
Ali Mohammadpour <aliemohammadpour@gmail.com>
2020-12-12 02:37:01 +0330

    Update Lab Material

---
+++ assignment-10/fsm.v
@@ -0,0 +1,38 @@
+/*--  *******************************************************
+--  Computer Architecture Course, Laboratory Sources 
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce[dot]aut[dot]ac[dot]ir
+--  *******************************************************
+--  All Rights reserved (C) 2019-2020
+--  *******************************************************
+--  Student ID  : 
+--  Student Name: 
+--  Student Mail: 
+--  *******************************************************
+--  Additional Comments:
+--
+--*/
+
+/*-----------------------------------------------------------
+---  Module Name: FSM
+---  Description: Lab 10 Part 2
+-----------------------------------------------------------*/
+`timescale 1 ns/1 ns
+
+module fsm (
+	input        rst ,
+	input        clk ,
+	input        confirm ,
+	input  [3:0] pass_data ,
+	output       en_left ,
+	output       en_right ,
+	output [3:0] dout
+);
+
+	/* write your code here */
+	
+	/* write your code here */
+
+endmodule
+
---
+++ assignment-10/register.v
@@ -0,0 +1,37 @@
+/*--  *******************************************************
+--  Computer Architecture Course, Laboratory Sources 
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce[dot]aut[dot]ac[dot]ir
+--  *******************************************************
+--  All Rights reserved (C) 2019-2020
+--  *******************************************************
+--  Student ID  : 
+--  Student Name: 
+--  Student Mail: 
+--  *******************************************************
+--  Additional Comments:
+--
+--*/
+
+/*-----------------------------------------------------------
+---  Module Name: Register 4 bit
+---  Description: Lab 10 Part 1
+-----------------------------------------------------------*/
+`timescale 1 ns/1 ns
+
+module register (
+
+	input        rst ,
+	input        clk ,
+	input        en ,
+	input  [3:0] din ,
+	output [3:0] qout
+);
+
+	/* write your code here */
+	
+	/* write your code here */
+
+endmodule
+
