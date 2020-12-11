b4292fc4bb21018ce47fd30accee9a13d8e8e758
Ali Mohammadpour <aliemohammadpour@gmail.com>
2020-12-11 14:42:06 +0330

    Update Lab Material

---
+++ assignment-09/tb_system.v
@@ -0,0 +1,39 @@
+/*--  *******************************************************
+--  Computer Architecture Course, Laboratory Sources 
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce[dot]aut[dot]ac[dot]ir
+--  *******************************************************
+--  All Rights reserved (C) 2019-2020
+--  *******************************************************
+--  Student ID  : 
+--  Student Name: 
+--  Student Mail: 
+--  *******************************************************
+--  Additional Comments:
+--
+--*/
+
+/*-----------------------------------------------------------
+---  Module Name: Sequential Circuit Testbench
+---  Description: Lab 09 Part 1 Testbench
+-----------------------------------------------------------*/
+`timescale 1 ns/1 ns
+
+
+module tb_adder_subtractor_4bit ();
+
+reg a;
+reg b;
+
+wire y;
+wire z;
+
+	initial 
+		begin
+		
+		// write your code here
+		
+	end
+
+endmodule
