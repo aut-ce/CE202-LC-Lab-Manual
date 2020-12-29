c248b536d21b90208cf7c282e9e270d5873b3cb4
Ali Mohammadpour <aliemohammadpour@gmail.com>
2020-12-29 21:02:24 +0330

    feat: Update Fall 2020 Project Material

---
+++ Projects/2020Fall/Module04/LightingSystemUtils.v
@@ -0,0 +1,29 @@
+/*--  *******************************************************
+--  Computer Architecture Course, Laboratory Sources 
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce[dot]aut[dot]ac[dot]ir
+--  *******************************************************
+--  All Rights reserved (C) 2019-2020
+--  *******************************************************
+--  Student ID  : 
+--  Student Name: 
+--  Student Mail: 
+--  *******************************************************
+--  Additional Comments:
+--
+--*/
+
+/*-----------------------------------------------------------
+---  Module Name: Lighting System Utils
+---  Description: Module4:
+-----------------------------------------------------------*/
+`timescale 1 ns/1 ns
+
+/***********************************************************/
+/************** Design Your Own Modules Below **************/
+
+
+	
+/************** Design Your Own Modules Above **************/
+/***********************************************************/
---
+++ Projects/2020Fall/Module04/WindowShadeDegree.v
@@ -0,0 +1,33 @@
+/*--  *******************************************************
+--  Computer Architecture Course, Laboratory Sources 
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce[dot]aut[dot]ac[dot]ir
+--  *******************************************************
+--  All Rights reserved (C) 2019-2020
+--  *******************************************************
+--  Student ID  : 
+--  Student Name: 
+--  Student Mail: 
+--  *******************************************************
+--  Additional Comments:
+--
+--*/
+
+/*-----------------------------------------------------------
+---  Module Name: Window Shade Degree
+---  Description: Module4: 
+-----------------------------------------------------------*/
+`timescale 1 ns/1 ns
+
+module WindowShadeDegree (
+	input  [3:0] tcode  , // time code    [table2 time code   ]
+	input  [3:0] ulight , // user light   [light degree mode  ]
+	output [3:0] wshade   // shade level  [window shade level ]
+);
+
+	/* write your code here */
+	
+	/* write your code here */
+
+endmodule
